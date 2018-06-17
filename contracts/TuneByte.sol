9dcc79e1e585c1212a34df17610a0e4b596d16ee
Ryan Stevens <stevens.ryan@gmail.com>
2018-06-16 23:54:51 -0700

    stuffs

--- contracts/TuneByte.sol
+++ contracts/TuneByte.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.4.18;
 contract TuneByte {
 
     constructor() public {
-
+        storedData = 1000;
     }
+
+    uint storedData;
+    function get() public view returns (uint) {
+      return storedData;
+    }
+    
 }
